123de732759b1f035442c6c7ce4e612751ddb315
Heric Branco <hericbranco@gmail.com>
2023-01-29 11:18:55 -0300

    refatoring to make CountriesIndexAction more independent

--- app/Actions/CountriesIndexAction.php
+++ app/Actions/CountriesIndexAction.php
@@ -3,12 +3,10 @@
 namespace App\Actions;
 
 use Illuminate\Support\Facades\Http;
-use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\ValidationException;
 use Illuminate\Support\Collection;
 use Illuminate\Http\Client\Response;
-use Throwable;
 use Illuminate\Support\Facades\Redis;
 
 
@@ -27,61 +25,31 @@ class CountriesIndexAction
         }
     }
 
-    private function getCountries(array $data) :Collection
+    private function getCountries(array $data): Collection
     {
-        $keys = array_keys($data);
-        sort($keys);
-        $stringKey = [];
-        array_walk($keys, function($key) use (&$stringKey, $data) {
-            $stringKey[] = $key.':'.$data[$key];
-        });
-        $stringKey = implode('|', $stringKey);
+        ksort($data);
+        $stringKey = serialize($data);
         $cached = Redis::get($stringKey);
         if ($cached) {
-            echo 'cached';
-            return collect(json_decode($cached));
+            return collect(unserialize($cached));
         }
 
         $http = Http::get('https://api.first.org/data/v1/countries', $data);
-        Redis::set($stringKey, json_encode($http->object()->data));
+        Redis::set($stringKey, serialize($http->object()->data));
         return collect($http->object()->data);
     }
 
     public function __invoke(array $data)
     {
-        try {
-            $this->validateData($data);
-            $data = array_merge($data, ['offset' => 0]);
-            if (!$data['limit']) {
-                $data['limit'] = 15;
-            }
-            $actualPage=1;
-            if ($data['page']) {
-                $actualPage = $data['page'];
-                $data['offset'] = ($data['page']-1)*$data['limit'];
-            }
-
-            $countries = $this->getCountries($data);
-            $queryString = array_map(function ($value, $key) {
-                return $key . '=' . $value;
-            }, request()->except('page'), array_keys(request()->except('page')));
-            $countries->path = request()->url() . '?';
-            if (!empty($queryString)) {
-                $countries->path = $countries->path . implode('&', $queryString) . '&';
-            }
-            
-            $countries->prev_page_url = ($actualPage > 1)?$countries->path.'page='.($actualPage-1):'';
-            $countries->next_page_url = ($countries->count() == $data['limit'])?$countries->path.'page='.($actualPage+1):'';
-
-            $countries->macro('links', function () {
-                return view('includes.pagination', ['data' => $this, 'show_page_links' => false]);
-            });
-            return responseOK('', $countries);
-        } catch (ValidationException $v) {
-            return responseFail($v->getMessage(), $v->errorBag->all());
-        } catch (Throwable $t) {
-            Log::critical($t->getMessage());
-            return responseFail($t->getMessage(), []);
+        $this->validateData($data);
+        $data = array_merge($data, ['offset' => 0]);
+        if (!$data['limit']) {
+            $data['limit'] = 15;
         }
+        if ($data['page']) {
+            $data['offset'] = ($data['page'] - 1) * $data['limit'];
+        }
+
+        return $this->getCountries($data);
     }
 }
--- app/Http/Controllers/Web/CountriesController.php
+++ app/Http/Controllers/Web/CountriesController.php
@@ -4,12 +4,51 @@ namespace App\Http\Controllers\Web;
 
 use App\Actions\CountriesIndexAction;
 use App\Http\Controllers\Controller;
+use Illuminate\Validation\ValidationException;
 
-class CountriesController extends Controller 
+use Illuminate\Support\Collection;
+use Illuminate\Http\Request;
+use Throwable;
+use Illuminate\Support\Facades\Log;
+
+class CountriesController extends Controller
 {
-    public function index(CountriesIndexAction $countriesIndexAction) 
+    private function transformToPaginatedCollection(Collection $collection, Request $request)
     {
-        $countries = $countriesIndexAction(request()->all(['q', 'region', 'page', 'limit']));
-        return view('countries.index', ['countries' => $countries]);
+        $actualPage = request('page') ?? 1;
+        $limit = request('limit') ?? 15;
+        $queryString = array_map(function ($value, $key) {
+            return $key . '=' . $value;
+        }, request()->except('page'), array_keys(request()->except('page')));
+        $collection->path = request()->url() . '?';
+        if (!empty($queryString)) {
+            $collection->path = $collection->path . implode('&', $queryString) . '&';
+        }
+
+        $collection->prev_page_url = ($actualPage > 1) ? $collection->path . 'page=' . ($actualPage - 1) : '';
+        $collection->next_page_url = ($collection->count() == $limit) ? $collection->path . 'page=' . ($actualPage + 1) : '';
+
+        $collection->macro('links', function () {
+            return view('includes.pagination', ['data' => $this, 'show_page_links' => false]);
+        });
+        return $collection;
+    }
+
+    private function getCountries(CountriesIndexAction $countriesIndexAction, Request $request)
+    {
+        try {
+            $countries = $countriesIndexAction(request()->all(['q', 'region', 'page', 'limit']));
+            return responseOk('', $this->transformToPaginatedCollection($countries, $request));
+        } catch (ValidationException $v) {
+            return responseFail($v->getMessage(), $v->errorBag->all());
+        } catch (Throwable $t) {
+            Log::critical($t->getMessage());
+            return responseFail($t->getMessage(), []);
+        }
+    }
+
+    public function index(CountriesIndexAction $countriesIndexAction)
+    {
+        return view('countries.index', ['countries' => $this->getCountries($countriesIndexAction, request())]);
     }
 }
